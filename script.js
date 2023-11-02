a67cbda297f0fa8193c4a7a8851d0feb3f3d23fc
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-02 14:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// const library = [
+//   {
+//     author: "Bill Gates",
+//     title: "The Road Ahead",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Steve Jobs",
+//     title: "Walter Isaacson",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Suzanne Collins",
+//     title: "Mockingjay: The Final Book of The Hunger Games",
+//     readingStatus: false,
+//   },
+// ];
+
+// const numberOfBooksRead = () => {
+//   // write your code here
+// };
+
+// // Do not change the code below
+
+// alert(numberOfBooksRead());
 const library = [
   {
     author: "Bill Gates",
@@ -16,10 +41,17 @@ const library = [
   },
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
+const numberOfBooksRead = (library) => {
+  let count = 0;
+  for (let book of library) {
+    if (book.readingStatus === true) {
+      count++;
+    }
+  }
+  return count;
 };
 
 // Do not change the code below
 
-alert(numberOfBooksRead());
+alert(numberOfBooksRead(library));
+
